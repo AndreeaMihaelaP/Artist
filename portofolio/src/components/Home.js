b05395f5aefda7333bf1940dab70fafd9fdb80a1
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2021-09-27 19:21:00 +0300

    WIP navigation

--- portofolio/src/components/Home.js
+++ portofolio/src/components/Home.js
@@ -5,7 +5,19 @@ const Home = () => {
   return (
     <div className="home">
       {/* <h3>Home</h3> */}
-      <div>{/* <p>Home</p> */}</div>
+      <div>
+        <p style={{ padding: "100px" }}>Home</p>
+        <p style={{ padding: "100px", fontSize: "40px", color: "whitesmoke" }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          faucibus urna ac tellus sollicitudin, ut tincidunt elit mattis.
+          Integer sed dignissim eros. Etiam nec tellus vitae lorem pellentesque
+          ullamcorper at vel eros.
+        </p>
+        {/* <p style={{ padding: "100px" }}>Home</p>
+        <p style={{ padding: "100px" }}>Home</p>
+        <p style={{ padding: "100px" }}>Home</p>
+        <p style={{ padding: "100px" }}>Home</p> */}
+      </div>
     </div>
   );
 };
--- portofolio/src/components/NavigationBar.js
+++ portofolio/src/components/NavigationBar.js
@@ -1,32 +1,56 @@
 import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import Logo from "../assets/logo/logo7.png";
+import "../styles.css";
 
 const NavigationBar = () => {
   return (
     <>
       <Navbar
         style={{
-          backgroundColor: "#00153E",
+          // backgroundColor: "black",
+          backgroundColor: "#001037",
           marginTop: "30px",
-          opacity: 0.5,
+          opacity: 0.8,
+          // opacity: 0.5,
         }}
         collapseOnSelect
         fixed="top"
         expand="sm"
+        // bg="dark"
         variant="dark">
         <Container>
-          <Navbar.Brand href="/">Brand</Navbar.Brand>
+          <Navbar.Brand
+            href="/"
+            style={{ display: "flex", alignItems: "center" }}>
+            <img
+              src={Logo}
+              alt="Logo"
+              style={{ width: "80px", height: "80px" }}
+            />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav>
-              <Nav.Link href="/">Home</Nav.Link>
-              <NavDropdown title="Gallery" id="basic-nav-dropdown">
+              <Nav.Link
+                href="/"
+                className="navbar-line px-5"
+                style={{ color: "white" }}>
+                HOME
+              </Nav.Link>
+              <NavDropdown
+                title="GALLERY"
+                id="basic-nav-dropdown"
+                className=" navbar-line px-5">
                 <NavDropdown.Item href="/gallery">Gallery</NavDropdown.Item>
                 <NavDropdown.Item href="/gallery">Flowers</NavDropdown.Item>
                 <NavDropdown.Item href="/gallery">Portraits</NavDropdown.Item>
                 <NavDropdown.Item href="/gallery">Landscapes</NavDropdown.Item>
               </NavDropdown>
-              <NavDropdown title="Exhibitions" id="basic-nav-dropdown">
+              <NavDropdown
+                title="EXHIBITIONS"
+                id="basic-nav-dropdown"
+                className="navbar-line px-5">
                 <NavDropdown.Item href="/exhibitions">
                   Personal
                 </NavDropdown.Item>
@@ -34,8 +58,12 @@ const NavigationBar = () => {
                   University
                 </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/about" className="navbar-line px-5">
+                ABOUT
+              </Nav.Link>
+              <Nav.Link href="/contact" className="px-5">
+                CONTACT
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
