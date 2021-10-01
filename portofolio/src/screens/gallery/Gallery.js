21138e99b58d2d069c3abb1a510de1b9858c1477
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2021-10-01 17:07:42 +0300

    Added cards

--- portofolio/src/screens/gallery/Gallery.js
+++ portofolio/src/screens/gallery/Gallery.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import { CardImage } from "components/gallery/CardImage";
+
+import "../../styles/gallery.css";
+import img1 from "assets/background.jpg";
+import img2 from "assets/img6.jpg";
+import img3 from "assets/img5.jpg";
+import img4 from "assets/2.jpg";
+import img5 from "assets/5.jpg";
 
 const Gallery = () => {
+  useEffect(() => {
+    console.log("mount");
+  }, []);
   return (
-    <div style={{ marginTop: "100px", textAlign: "center" }}>
-      <h3>Gallery</h3>
-      <div>{/* <p>Gallery</p> */}</div>
+    <div className="gallery-container">
+      <div className="gallery-content">
+        <h3 className="gallery-description">
+          Welcome to our gallery.
+          <p>
+            Every painting is a story of healing, of seeking "love-tings" and
+            "comfort-tings."
+          </p>
+        </h3>
+        <Container>
+          <Row>
+            <Col>
+              <CardImage image={img1} title="Static Nature" />
+            </Col>
+            <Col>
+              <CardImage image={img2} title="Portrete" />
+            </Col>
+            <Col>
+              <CardImage image={img5} title="Abstract" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <CardImage image={img4} title="Peisaj" />
+            </Col>
+            <Col>
+              <CardImage image={img3} title="Icoane" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
